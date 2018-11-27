574fffe43547012ca62fe620c98e39855e839a2c
WJC <jichao.wang@ly.com>
2018-11-27 15:40:20 +0800

    init

---
+++ flink/src/main/java/wjc920/flink/App.java
@@ -0,0 +1,13 @@
+package wjc920.flink;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
