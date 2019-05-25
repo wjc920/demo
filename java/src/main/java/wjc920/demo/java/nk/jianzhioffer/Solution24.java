16337d8f3ed7d375a2be4237fdc1d4b8ff4b0f65
wjc920@foxmail.com <wjc920@foxmail.com>
2019-05-25 22:24:14 +0800

    第8天 剑指Offer 24

--- java/src/main/java/wjc920/demo/java/nk/jianzhioffer/Solution24.java
+++ java/src/main/java/wjc920/demo/java/nk/jianzhioffer/Solution24.java
@@ -1,22 +1,40 @@
 package wjc920.demo.java.nk.jianzhioffer;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
+ * 二叉树中和为某一值的路径
  *
- *
- * @author jichao.wang
- * @date 2019年05月22日 下午05:53:13
+ * @author wjc
+ * @date 2019年05月25日 下午10:20:31
  */
 public class Solution24 {
 
-    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
-        if(root==null){
-            return null;
-        }
-        if(target==0){
+    ArrayList<ArrayList<Integer>> result = new ArrayList<>();
 
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+        traverse(root, new ArrayList<>(), target);
+        return result;
+    }
+
+    public void traverse(TreeNode root, ArrayList<TreeNode> t, int target) {
+        if (root == null) {
+            return;
         }
-        return null;
+        target -= root.val;
+        t.add(root);
+        if (root.left == null && root.right == null && target == 0) {
+            ArrayList<Integer> tVal = new ArrayList<>();
+            for (TreeNode tmp : t) {
+                tVal.add(tmp.val);
+            }
+            result.add(tVal);
+        } else {
+            traverse(root.left, t, target);
+            traverse(root.right, t, target);
+        }
+        target += root.val;
+        t.remove(root);
     }
 }
